6395475a9e7a38be96b6438b4e82059dd81d10e6
dhiksn <andhikarafi321@gmail.com>
2026-06-25 20:09:50 +0700

    deploy to vercel

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ app.post('/api/iqc', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
